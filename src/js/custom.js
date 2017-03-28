e76c31cbd6965e4edc846fc9c0edf680888d64bc
Grace <gracefctorres@gmail.com>
2017-03-28 17:51:42 -0500

    fixing style

--- src/js/custom.js
+++ src/js/custom.js
@@ -2,12 +2,13 @@ $(document).ready(init);
 
 function init(){
     var btn =$('#btn_info').click(onClickInfo);
+    
 }
 function onClickInfo(){
-    var origen=$('#city_origen').val();
-    alert('origen');
-    var destino=$('#city_destiny').attr('value');
-    if(origen==0 && destino==0){
+    var origen=$('.city_origen').val();
+    var destino=$('.city_destino').val();
+    
+    if(origen=="" && destino==""){
         alert("elige un destino");
     }
 }
